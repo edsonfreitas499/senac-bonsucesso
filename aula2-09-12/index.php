2ec2cfca22dd438e4b6c42c77d1292b00676fba3
edsom <edsonfreitas499@gmail.com>
2019-12-09 21:17:32 -0300

    aula 2 php

---
+++ aula2-09-12/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title></title>
+</head>
+<body>
+	<h1>
+		<?php 
+		if(isset($_GET["user"])) {
+			echo base64_decode($_GET["user"]);
+		} else {
+			header("location:formulario.php");
+		}
+	?>
+</h1>	
+	
+</body>
+</html>	
